22533bb00e496a2a4548617e2f6578dab887e0cc
HCKgit <hckilburn@gmail.com>
2018-06-14 22:50:38 -0300

    Writing your first Kotlin controller fro the tutorial https://spring.io/guides/tutorials/spring-boot-kotlin/

---
+++ src/main/kotlin/com/example/blog/HtmlController.kt
@@ -0,0 +1,21 @@
+package com.example.blog
+
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.ui.set
+import org.springframework.web.bind.annotation.GetMapping
+
+@Controller
+class HtmlController {
+
+    @GetMapping("/")
+    fun blog(model: Model): String {
+        // Here we import the org.springframework.ui.set extension function in order to be able to write
+        // model["title"] = "Blog"
+        // instead of
+        // model.addAttribute("title", "Blog").
+        model["title"] = "Blog"
+        return "blog"
+    }
+
+}
